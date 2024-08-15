3411730d94effaf9cb05b79803e036e04ce7ba28
VJS1805 <vishvauday786@gmail.com>
2024-08-15 11:57:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+// result ='3' - 2;
+// result=String(result);
+// console.log(result);
+// console.log(typeof result);
+// console.log(5 ** 2);
+// //comparison operators
+// a = 2;
+// b = 3;
+// console.log(a==b)
+// console.log(a!=b)
+// console.log(a<b)
+// console.log(a>b)
+// console.log(a<=b)
+// console.log(a>=b)
+// console.log(a===b)
+  
+// // logical OR
+// console.log(4>3 || 3>4 );
+// // logical And
+// console.log(4>3 && 3>4 );
+// //logical not
+// console.log(!true)
+
+//conditional
+// let age=15;
+// let name="VJS";
+
+// if(age>=18){
+//     if (name=="VJS"){
+//     console.log("you are eligible");
+// }
+// else{
+//     console.log("Name is not in match")
+// }
+// }
+// else{
+//     console.log("You are not eligible")
+// }
+
+// Else if
+// let a=10;
+// let b=15;
+
+// if (a>b) {
+//     console.log("A is greater");    
+// }
+// else if (b>a) {
+//     console.log("B is greater");
+// }
+// else{
+//     console.log("Both are equal");
+// }
